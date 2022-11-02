e1bd173d986caa7aa7ec8db5f5b7c2e0c79bb931
david <dayveed1@users.noreply.github.com>
2022-11-02 13:48:10 +0100

    Adding Inheritance

---
+++ Inheritance.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.4.16 <0.9.0;
+//SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/// My Contract
+contract MyContract is ERC20 {
+    constructor() payable ERC20("Dave's TOKEN", "DAVZ") {
+        // fixed supply using internal _mint function
+        // here 9900 DAVZ initial supply 
+        _mint(msg.sender, 9900);
+    }
+
+    // no decimals
+    function decimals() public view virtual override returns (uint8) {
+        return 0;
+    }
+
+    // to support receiving ETH by default
+    receive() external payable {}
+
+    fallback() external payable {}
+}
+
+// My Contract 0xd9145CCE52D386f254917e481eB44e9943F39138
